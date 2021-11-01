168656b62413e2207b51eee325c5d203e4198609
Blazej Nagiecki <blazej.nagiecki@gmail.com>
2021-11-01 14:37:41 +0100

    add if/else

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} 
+    else if (1 == 2) {
+    computerMove = 'papier';
+}
+
+    else if (1 == 3) {
+    computerMove = 'nożyce';
+}
+
