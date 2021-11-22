2f1aef75336b33f52de394aad9e4e10bb5580768
Blazej Nagiecki <blazej.nagiecki@gmail.com>
2021-11-22 16:29:00 +0100

    clear script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 //guziki
 function buttonClicked(argPlayerMove) {
   clearMessages();
-  console.log(argPlayerMove + " został kliknięty");
 
   const randomNumber = Math.floor(Math.random() * 3 + 1);
   const computerMove = getMoveName(randomNumber);
@@ -62,37 +61,3 @@ const buttonScissors = document.getElementById("button-scissors");
 buttonScissors.addEventListener("click", function () {
   buttonClicked("nożyce");
 });
-
-//komputer//
-//var computerMove, randomNumber;
-//randomNumber = Math.floor(Math.random() * 3 + 1);
-//console.log('wylosowana liczba to: ' + randomNumber);
-//
-//if (randomNumber == '1') {
-//    computerMove = 'kamień';
-//}
-//    else if (randomNumber == '2') {
-//    computerMove = 'papier';
-//}
-//    else if (randomNumber == '3') {
-//    computerMove = 'nożyce';
-//}
-
-//gracz//
-//var playerMove, playerInput;
-//playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-//console.log('Wpisana odpowiedź to: ' + playerInput);
-//
-//if (playerInput == '1') {
-//    playerMove = 'kamień';
-//}
-//    else if (playerInput  == '2') {
-//    playerMove = 'papier';
-//}
-//    else if (playerInput == '3') {
-//    playerMove = 'nożyce';
-//}
-//    else {
-//    playerMove = 'nieznany ruch';
-//    printMessage('Błędny wybór!' + playerMove);
-//
